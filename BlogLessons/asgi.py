31922380c3c21b3d379ecb22f252e1b28f617262
Odimac84 <bjorn.nilsson1984@gmail.com>
2021-10-17 20:22:48 +0000

    initial commit

---
+++ BlogLessons/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BlogLessons project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BlogLessons.settings')
+
+application = get_asgi_application()
